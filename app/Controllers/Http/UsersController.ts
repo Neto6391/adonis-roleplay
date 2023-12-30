9201216d5ec45e6bb527c9a977d195c010e6f92b
Neto6391 <neto6391@gmail.com>
2023-12-30 16:07:27 -0300

    feat(create a user and tests units)

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,19 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequestException from 'App/Exceptions/BadRequestException'
+import User from 'App/Models/User'
+import CreateUser from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
-  public async store({ response }) {
-    return response.created({})
+  public async store({ request, response }: HttpContextContract) {
+    const userPayload = await request.validate(CreateUser)
+
+    const userByUsername = await User.findBy('username', userPayload.username)
+    const userByEmail = await User.findBy('email', userPayload.email)
+
+    if (userByUsername) throw new BadRequestException('username already in use', 409)
+    if (userByEmail) throw new BadRequestException('email already in use', 409)
+
+    const user = await User.create(userPayload)
+    return response.created({ user })
   }
 }
