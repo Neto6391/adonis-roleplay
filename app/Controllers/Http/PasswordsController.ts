ddd1cdb23bbe6e31d870d71e27776fbe526838dd
Neto6391 <neto6391@gmail.com>
2023-12-31 20:25:08 -0300

    feat(create controller for route forgot-password and unit tests)

---
+++ app/Controllers/Http/PasswordsController.ts
@@ -0,0 +1,39 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
+
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import ForgotPassword from 'App/Validators/ForgotPasswordValidator'
+
+import { randomBytes } from 'crypto'
+import { promisify } from 'util'
+
+export default class PasswordsController {
+  public async forgotPassword({ request, response }: HttpContextContract) {
+    const { email, resetPasswordUrl } = await request.validate(ForgotPassword)
+    const user = await User.findByOrFail('email', email)
+
+    const random = await promisify(randomBytes)(24)
+    const token = random.toString('hex')
+    await user.related('tokens').updateOrCreate(
+      { userId: user.id },
+      {
+        token,
+      }
+    )
+
+    const resetPasswordUrlWithToken = `${resetPasswordUrl}?token=${token}`
+    await Mail.send((message) => {
+      message
+        .from('no-reply@roleplay.com')
+        .to(email)
+        .subject('Roleplay: Recuperação de Senha')
+        .htmlView('email/forgotpassword', {
+          productName: 'Roleplay',
+          name: user.username,
+          resetPasswordUrl: resetPasswordUrlWithToken,
+        })
+    })
+
+    return response.noContent()
+  }
+}
---
+++ test/users/password.spec.ts
@@ -0,0 +1,66 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Password', (group) => {
+  test('it should send and email with forgot password instructions', async (assert) => {
+    const user = await UserFactory.create()
+
+    Mail.trap((message) => {
+      assert.deepEqual(message.to, [
+        {
+          address: user.email,
+        },
+      ])
+      assert.deepEqual(message.from, {
+        address: 'no-reply@roleplay.com',
+      })
+      assert.equal(message.subject, 'Roleplay: Recuperação de Senha')
+      assert.include(message.html!, user.username)
+    })
+
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: 'url',
+      })
+      .expect(204)
+
+    Mail.restore()
+  })
+
+  test('it should create a reset password token', async (assert) => {
+    const user = await UserFactory.create()
+
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: 'url',
+      })
+      .expect(204)
+
+    const tokens = await user.related('tokens').query()
+
+    assert.isNotEmpty(tokens)
+  })
+
+  test('it should return 422 when requried data is not provided or data is invalid', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/forgot-password').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
