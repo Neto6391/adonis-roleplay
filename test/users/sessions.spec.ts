b45186a5c368ca19dc0528465199cdc86ebfaa5e
Neto6391 <neto6391@gmail.com>
2024-01-02 12:39:27 -0300

    feat(create controller for sessions and begin unit tests)

---
+++ test/users/sessions.spec.ts
@@ -0,0 +1,19 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Session', async (group) => {
+  test.only('it should authenticate an user', async () => {
+    await supertest(BASE_URL).post('/sessions').send({}).expect(201)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
