f3828403e16141fac8c88f2742bbca6ca4bf3177
Neto6391 <neto6391@gmail.com>
2024-01-02 00:48:37 -0300

    feat(add password controller reset-password)

--- test/users/password.spec.ts
+++ test/users/password.spec.ts
@@ -1,7 +1,9 @@
 import Mail from '@ioc:Adonis/Addons/Mail'
+import Hash from '@ioc:Adonis/Core/Hash'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { UserFactory } from 'Database/factories'
 import test from 'japa'
+import { DateTime, Duration } from 'luxon'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
@@ -54,6 +56,63 @@ test.group('Password', (group) => {
     const { body } = await supertest(BASE_URL).post('/forgot-password').send({}).expect(422)
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 422)
+  })
+
+  test('it should be able to reset password', async (assert) => {
+    const user = await UserFactory.create()
+    const { token } = await user.related('tokens').create({
+      token: 'token',
+    })
+    await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(204)
+    await user.refresh()
+    const checkPassword = await Hash.verify(user.password, '123456')
+    assert.isTrue(checkPassword)
+  })
+
+  test('it should return 422 when requried data is not provided or data is invalid', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/reset-password').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
+  test('it should return 404 when using the same token twice', async (assert) => {
+    const user = await UserFactory.create()
+
+    const { token } = await user.related('tokens').create({
+      token: 'token',
+    })
+    await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(204)
+
+    const { body } = await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(404)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 404)
+  })
+
+  test('it cannot reset password when token is expired after 2 hours', async (assert) => {
+    const user = await UserFactory.create()
+    const date = DateTime.now().minus(Duration.fromISOTime('02:01'))
+    const { token } = await user.related('tokens').create({
+      token: 'token',
+      createdAt: date,
+    })
+
+    const { body } = await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(410)
+
+    assert.equal(body.code, 'TOKEN_EXPIRED')
+    assert.equal(body.status, 410)
+    assert.equal(body.message, 'token has expired')
   })
 
   group.beforeEach(async () => {
