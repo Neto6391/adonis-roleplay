d7d2a1f8a71094b0fabbc2dafb3d24e5ef90e39b
Neto6391 <neto6391@gmail.com>
2024-01-08 12:18:14 -0300

    chore(add configuration to database MySQL)

--- config/database.ts
+++ config/database.ts
@@ -6,11 +6,12 @@
  */
 
 import Env from '@ioc:Adonis/Core/Env'
-import Application from '@ioc:Adonis/Core/Application'
+
 import type { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+import Application from '@ioc:Adonis/Core/Application'
 
 const databaseConfig: DatabaseConfig = {
-  /*
+    /*
   |--------------------------------------------------------------------------
   | Connection
   |--------------------------------------------------------------------------
@@ -20,39 +21,54 @@ const databaseConfig: DatabaseConfig = {
   | file.
   |
   */
-  connection: Env.get('DB_CONNECTION'),
+    connection: Env.get('DB_CONNECTION'),
 
-  connections: {
-    /*
+    connections: {
+        /*
     |--------------------------------------------------------------------------
-    | SQLite
+    | MySQL config
     |--------------------------------------------------------------------------
     |
-    | Configuration for the SQLite database.  Make sure to install the driver
+    | Configuration for MySQL database. Make sure to install the driver
     | from npm when using this connection
     |
-    | npm i sqlite3
+    | npm i mysql2
     |
     */
-    sqlite: {
-      client: 'sqlite',
-      connection: {
-        filename: Application.tmpPath('db.sqlite3'),
-      },
-      pool: {
-        afterCreate: (conn, cb) => {
-          conn.run('PRAGMA foreign_keys=true', cb)
-        }
-      },
-      migrations: {
-        naturalSort: true,
-      },
-      useNullAsDefault: true,
-      healthCheck: false,
-      debug: false,
-    },
+        mysql: {
+            client: 'mysql2',
+            connection: {
+                host: Env.get('MYSQL_HOST'),
+                port: Env.get('MYSQL_PORT'),
+                user: Env.get('MYSQL_USER'),
+                password: Env.get('MYSQL_PASSWORD', ''),
+                database: Env.get('MYSQL_DB_NAME'),
+            },
+            migrations: {
+                naturalSort: true,
+            },
+            healthCheck: false,
+            debug: false,
+        },
 
-  }
+        sqlite: {
+            client: 'sqlite',
+            connection: {
+                filename: Application.tmpPath('db.sqlite3'),
+            },
+            pool: {
+                afterCreate: (conn, cb) => {
+                    conn.run('PRAGMA foreign_keys=true', cb)
+                },
+            },
+            migrations: {
+                naturalSort: true,
+            },
+            useNullAsDefault: true,
+            healthCheck: false,
+            debug: false,
+        },
+    },
 }
 
 export default databaseConfig
