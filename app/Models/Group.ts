ca9426827181a2767cee08b46e0865b50e77092c
Neto6391 <neto6391@gmail.com>
2024-01-08 12:21:16 -0300

    feat(create scopes for query building to improves queries filter in get in model use)

--- app/Models/Group.ts
+++ app/Models/Group.ts
@@ -1,49 +1,63 @@
 import { DateTime } from 'luxon'
 import {
-  BaseModel,
-  BelongsTo,
-  ManyToMany,
-  belongsTo,
-  column,
-  manyToMany,
+    BaseModel,
+    BelongsTo,
+    ManyToMany,
+    ModelQueryBuilderContract,
+    belongsTo,
+    column,
+    manyToMany,
+    scope,
 } from '@ioc:Adonis/Lucid/Orm'
 import User from 'App/Models/User'
 
+type Builder = ModelQueryBuilderContract<typeof Group>
+
 export default class Group extends BaseModel {
-  @column({ isPrimary: true })
-  public id: number
+    @column({ isPrimary: true })
+    public id: number
 
-  @column()
-  public name: string
+    @column()
+    public name: string
 
-  @column()
-  public description: string
+    @column()
+    public description: string
 
-  @column()
-  public schedule: string
+    @column()
+    public schedule: string
 
-  @column()
-  public location: string
+    @column()
+    public location: string
 
-  @column()
-  public chronic: string
+    @column()
+    public chronic: string
 
-  @column()
-  public master: number
+    @column()
+    public master: number
 
-  @belongsTo(() => User, {
-    foreignKey: 'master',
-  })
-  public masterUser: BelongsTo<typeof User>
+    @belongsTo(() => User, {
+        foreignKey: 'master',
+    })
+    public masterUser: BelongsTo<typeof User>
 
-  @manyToMany(() => User, {
-    pivotTable: 'groups_users',
-  })
-  public players: ManyToMany<typeof User>
+    @manyToMany(() => User, {
+        pivotTable: 'groups_users',
+    })
+    public players: ManyToMany<typeof User>
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+    @column.dateTime({ autoCreate: true })
+    public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    public updatedAt: DateTime
+
+    public static withPlayer = scope((query: Builder, userId: number) => {
+        query.whereHas('players', (query) => {
+            query.where('id', userId)
+        })
+    })
+
+    public static withText = scope((query: Builder, text: string) => {
+        query.where('name', 'LIKE', `%${text}%`).orWhere('description', 'like', `%${text}%`)
+    })
 }
